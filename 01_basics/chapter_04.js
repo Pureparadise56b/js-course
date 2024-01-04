11a3438b7dac7329eb1e99b313e9c9055e26ae53
Toufique Al Ajay <87595397+Pureparadise56b@users.noreply.github.com>
2024-01-04 10:40:23 +0000

    4th chapter

---
+++ 01_basics/chapter_04.js
@@ -0,0 +1,30 @@
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let string1 = "hello";
+let string2 = " ajay";
+
+// console.log(string1 + string2);
+
+//some wiered conversiton
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "3");
+
+// increament operations
+
+let counter = 400;
+
+console.log(++counter); //prefix increament then return value
+console.log(counter++); //postfix  return value then increament
+console.log(counter);
