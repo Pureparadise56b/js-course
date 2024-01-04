fa27a3713cefdd5afe44107eefaf7022a595785c
Toufique Al Ajay <87595397+Pureparadise56b@users.noreply.github.com>
2024-01-04 11:11:10 +0000

    2nd chapter

--- 01_basics/chapter_02.js
+++ 01_basics/chapter_02.js
@@ -1,5 +1,7 @@
 "use strict"; //treat all JS code as newer version.
 
+/* Datatypes */
+
 // alert(3 + 3) //we are usign nodejs, not browser so we can't perform anything related document in javascript.
 
 //Prioritize code readability.
@@ -28,3 +30,49 @@ let isLoggedIn = true;
 // return type of any variable
 console.log(typeof undefined); // undefined
 console.log(typeof null); // object
+
+/* Datatypes are mainly two types basis on memory allocation and access type */
+
+// 1st: Premetive (they are call by value)
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// declare Symbol
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id == anotherId);
+
+//declare BigInt
+
+const bigNumber = 93573475349739473297534n;
+console.log(bigNumber);
+
+// 2nd: Reference (Non premetive)
+// 3types: Array, Objects, Functions
+
+// Array
+const heros = ["shaktiman", "naagraj", "krish"];
+
+// Objects
+const details = {
+  name: "Ajay",
+  age: 20,
+  id: 1001,
+};
+
+console.log(typeof details);
+
+/*
+Type                  return
+-----	              ------
+Undefined	        "undefined"
+Null	            "object" (reason)
+Boolean	            "boolean"
+Number	            "number"
+BigInt	            "bigint"
+String	            "string"
+Symbol	            "symbol"
+Function 	        "function"
+Any other object	"object"
+*/
