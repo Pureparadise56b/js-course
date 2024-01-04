860791cbeb4ad2ee788502f49d7de205121db464
Toufique Al Ajay <87595397+Pureparadise56b@users.noreply.github.com>
2024-01-04 09:29:13 +0000

    commit 04012024

---
+++ 01_basics/chapter_02.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version.
+
+// alert(3 + 3) //we are usign nodejs, not browser so we can't perform anything related document in javascript.
+
+//Prioritize code readability.
+
+// console.log(3
+//     +
+//      3); console.log("ajay") // Wrong format ❌
+
+// console.log(3 + 3)
+// console.log("ajay")  // Correct format ✅
+
+let name = "ajay";
+let age = 20;
+let isLoggedIn = true;
+
+/* Premetive Datatype */
+
+// number => max 2 to the power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => datndalone value
+// undefined
+// symbol => unique
+
+// return type of any variable
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
