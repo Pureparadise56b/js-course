8ce62e5df02d8d31d8d9578dd9efd8a9ec573386
Toufique Al Ajay <87595397+Pureparadise56b@users.noreply.github.com>
2024-01-05 13:09:56 +0000

    chapter 1

--- 02_basics/chapter_01.js
+++ 02_basics/chapter_01.js
@@ -1,35 +1,79 @@
-// arrays
+/* arrays */
 
 // () -> parentheses, [] -> square brackets, {} -> curly braces
 
-const myArr = [0, 1, 2, 3, 4, 5, "ajay", true, 6.5];
+const myArr = [0, 1, 2, 3, 4, 5, "ajay", true, 6.5]; // 1st declaration
 // console.log(myArr);
 
-const myArr2 = new Array("ajay", "sujay", "bijoy");
+const myArr2 = new Array("ajay", "sujay", "bijoy"); // 2nd declaration
 // console.log(myArr2);
 
-// array methods
+/* array methods */
 
 // myArr.push(7);
 // myArr.pop();
-// myArr.unshift(10);
-// myArr.shift();
-// console.log(myArr.includes(8));
-// console.log(myArr.indexOf(8));
+// myArr.unshift(10); //insert element in the first position
+// myArr.shift(); //remove element from the first position
+// console.log(myArr.includes(8)); //return false
+// console.log(myArr.indexOf(8)); // return -1
 
-// const newArr = myArr.join();
-// console.log(newArr);
+// console.log(myArr);
 
-// slice & splice
+const separateArr = myArr.join("-");
+// console.log(separateArr);
+
+/* slice & splice */
 
 const newArr = [0, 1, 2, 3, 4, 5];
 
-console.log("A: ", newArr);
+// console.log("A: ", newArr);
 
-const nn1 = newArr.slice(1, 4);
-console.log(nn1);
-console.log("B: ", newArr);
+const nn1 = newArr.slice(1, 4); // return sliced array of specified index but don't change real array
+// console.log(nn1);
+// console.log("B: ", newArr);
 
-const nn2 = newArr.splice(1, 4);
-console.log(nn2);
-console.log("C: ", newArr);
+const nn2 = newArr.splice(1, 4); // actually removed sliced items form the real array
+// console.log(nn2);
+// console.log("C: ", newArr);
+
+const marvalHeros = ["spiderman", "ironman", "thor", "hulk"];
+const dcHeros = ["superman", "batman", "flash", "cyborck", "catgirl"];
+
+// dcHeros.push(marvalHeros);
+// console.log(dcHeros);
+
+const allHeros = marvalHeros.concat(dcHeros);
+// console.log(allHeros);
+
+/* spread operator */
+
+const anotherAllHeros = [...marvalHeros, ...dcHeros];
+// console.log(anotherAllHeros);
+
+const numbers = [
+  5,
+  6,
+  7,
+  [10, 40, 30],
+  8,
+  9,
+  [1, 2, [100, [6000, 8000], 200, 500], 3, 4],
+];
+
+// console.log(numbers.flat(Infinity));
+
+// console.log(Array.from("Toufique Al Ajay"));
+
+// console.log(
+//   Array.from({
+//     name: "ajay",
+//     age: 19,
+//     id: 1003,
+//   })
+// );
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+// console.log(Array.of(score1, score2, score3));
