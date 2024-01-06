ce7984b013a7e2acdf95840e8b9d7a0da2f1a225
Toufique Al Ajay <87595397+Pureparadise56b@users.noreply.github.com>
2024-01-06 06:10:45 +0000

    chapter 3

--- 02_basics/chapter_03.js
+++ 02_basics/chapter_03.js
@@ -13,3 +13,38 @@ function loginUserMessage(username = "ajay") {
 }
 
 console.log("Message: ", loginUserMessage("toufique"));
+
+/* function with rest operator to accept n numbers of parametes */
+
+function calculateCartPrice(...num1) {
+  return num1;
+}
+
+function calculateCartPrice2(val1, val2, val3, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(2, 3, 4, 5, 6, 7, 8, 9, 10));
+// console.log(calculateCartPrice2(2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+/* function with objects */
+
+const item = {
+  name: "iphone",
+  price: 80000,
+  mft: "apple PVT. LTD",
+  inStock: true,
+};
+
+function showItems(obj) {
+  console.log(`The item you carted is ${obj.name}`);
+  console.log(`Price is ${obj.price}`);
+  console.log(`Manufacturer ${obj.mft}`);
+  if (obj.inStock) {
+    console.log("In stock");
+  } else {
+    console.log("Out of stock");
+  }
+}
+
+showItems(item);
